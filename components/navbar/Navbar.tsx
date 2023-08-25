d654277dd291a579db2dc4d85cd38e232418317f
Jared Brown <jaredbrown1@hotmail.com>
2023-08-24 21:14:13 -0700

    basic mobile navbar styling done

---
+++ components/navbar/Navbar.tsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+
+const Navbar = () => {
+	return (
+		<div className="flex flex-shrink-0 justify-between w-[375px] h-[64px] pt-4">
+			<div className="flex cursor-pointer h-[25px]">
+				<div className="pl-4 pt-4">
+					<Image src="/logo-mobile.svg" width={24} height={25} alt="logo" />
+				</div>
+
+				<h1 className="flex pt-4 pl-4 text-lg font-bold">
+					Platform Launch
+					<span className="mt-2 pl-2">
+						<Image
+							src="/icon-chevron-down.svg"
+							width={8}
+							height={4}
+							alt="down arrow"
+						/>
+					</span>
+				</h1>
+			</div>
+			<div className="flex p-4">
+				<button className="w-[48px] h-[32px] bg-[#635FC7] rounded-2xl pl-[19px] opacity-[0.25]">
+					<span>
+						<Image
+							src="/icon-add-task-mobile.svg"
+							width={10}
+							height={10}
+							alt="add column"
+						/>
+					</span>
+				</button>
+				<div className="p-2">
+					<Image
+						src="/icon-vertical-ellipsis.svg"
+						width={3}
+						height={16}
+						alt="user"
+					/>
+				</div>
+			</div>
+		</div>
+	);
+};
+export default Navbar;
